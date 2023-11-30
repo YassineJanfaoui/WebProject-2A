b5fd20aef49456643652de25dd5bd096176117fa
YassineJanfaoui <47060368+YassineJanfaoui@users.noreply.github.com>
2023-11-30 01:25:08 +0100

    added alert for successful feedback

--- View/AddFeedback.php
+++ View/AddFeedback.php
@@ -13,24 +13,21 @@ session_start();
 
 if (isset($_SESSION["user_id"]) && isset($_POST["description"]) && isset($_POST["stars"])) //input control
 {
-    if (!empty($_SESSION["user_id"]) && !empty($_POST["description"]) ) {
-      if ($userC->checkUserID($_SESSION["user_id"])!=null)
-      {
-        $date = date('Y-m-d');
-        $feedback = new Feedback(null, $_SESSION["user_id"], $_POST["description"],$date,$_POST["stars"]);    //adding values to new feedback object
-        $feedbackC->addfeedback($feedback);
-        $success = 1;
-      }
-      else
-      {
-        $error = "User ID not found";
-        $success = 0;
-      }
+  if (!empty($_SESSION["user_id"]) && !empty($_POST["description"])) {
+    if ($userC->checkUserID($_SESSION["user_id"]) != null) {
+      $date = date('Y-m-d');
+      $feedback = new Feedback(null, $_SESSION["user_id"], $_POST["description"], $date, $_POST["stars"]);    //adding values to new feedback object
+      $feedbackC->addfeedback($feedback);
+      $success = 1;
     } else {
-        $error = "Missing information";
-        $success = 0;
+      $error = "User ID not found";
+      $success = 0;
     }
-} 
+  } else {
+    $error = "Missing information";
+    $success = 0;
+  }
+}
 
 ?>
 
@@ -41,121 +38,117 @@ if (isset($_SESSION["user_id"]) && isset($_POST["description"]) && isset($_POST[
 <html>
 
 <head>
-    <title>Freud Clinic</title>
-    <link rel="stylesheet" href="../assets/css/maicons.css">
-    <link rel="stylesheet" href="../assets/css/bootstrap.css">
-    <link rel="stylesheet" href="../assets/vendor/owl-carousel/css/owl.carousel.css">
-    <link rel="stylesheet" href="../assets/vendor/animate/animate.css">
-    <link rel="stylesheet" href="../assets/css/theme.css">
-    <link rel="stylesheet" href="../styles/styleFeedback.css" />
+  <title>Freud Clinic</title>
+  <link rel="stylesheet" href="../assets/css/maicons.css">
+  <link rel="stylesheet" href="../assets/css/bootstrap.css">
+  <link rel="stylesheet" href="../assets/vendor/owl-carousel/css/owl.carousel.css">
+  <link rel="stylesheet" href="../assets/vendor/animate/animate.css">
+  <link rel="stylesheet" href="../assets/css/theme.css">
+  <link rel="stylesheet" href="../styles/styleFeedback.css" />
 </head>
 
 <body>
   <div class="overlay" id="blur">
-<div class="back-to-top"></div>
+    <div class="back-to-top"></div>
 
-<header>
-  <div class="topbar">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-8 text-sm">
-          <div class="site-info">
-            <a href="#"><span class="mai-call text-primary"></span> +00 123 4455 6666</a>
-            <span class="divider">|</span>
-            <a href="#"><span class="mai-mail text-primary"></span> mail@example.com</a>
-          </div>
-        </div>
-        <div class="col-sm-4 text-right text-sm">
-          <div class="social-mini-button">
-            <a href="#"><span class="mai-logo-facebook-f"></span></a>
-            <a href="#"><span class="mai-logo-twitter"></span></a>
-            <a href="#"><span class="mai-logo-dribbble"></span></a>
-            <a href="#"><span class="mai-logo-instagram"></span></a>
-          </div>
-        </div>
-      </div> <!-- .row -->
-    </div> <!-- .container -->
-  </div> <!-- .topbar -->
+    <header>
+      <div class="topbar">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-8 text-sm">
+              <div class="site-info">
+                <a href="#"><span class="mai-call text-primary"></span> +00 123 4455 6666</a>
+                <span class="divider">|</span>
+                <a href="#"><span class="mai-mail text-primary"></span> mail@example.com</a>
+              </div>
+            </div>
+            <div class="col-sm-4 text-right text-sm">
+              <div class="social-mini-button">
+                <a href="#"><span class="mai-logo-facebook-f"></span></a>
+                <a href="#"><span class="mai-logo-twitter"></span></a>
+                <a href="#"><span class="mai-logo-dribbble"></span></a>
+                <a href="#"><span class="mai-logo-instagram"></span></a>
+              </div>
+            </div>
+          </div> <!-- .row -->
+        </div> <!-- .container -->
+      </div> <!-- .topbar -->
 
-  <nav class="navbar navbar-expand-lg navbar-light shadow-sm">
-    <div class="container">
-      <a class="navbar-brand" href="index.php"><span class="text-primary">One</span>-Health</a>
+      <nav class="navbar navbar-expand-lg navbar-light shadow-sm">
+        <div class="container">
+          <a class="navbar-brand" href="index.php"><span class="text-primary">One</span>-Health</a>
 
-      <div class="collapse navbar-collapse" id="navbarSupport">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="index.php">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="AddFeedback.php">Feedback</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="about.php">About Us</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="doctors.php">Doctors</a>
-          </li>
-          <?php
-if(!isset($_SESSION["user_id"]) || empty($_SESSION["user_id"]))
-{
-  echo '<li class="nav-item"><a class="btn btn-primary ml-lg-3" href="login.php">Login / Register</a></li>';
-}
-else
-{
-  echo '<li class="nav-item"><a class="nav-link" href="#">username:';
-  echo $_SESSION["username"];
-  echo '</a></li>';
-  echo '<li class="nav-item"><a class="btn btn-primary ml-lg-3" href="logout.php">LogOut</a></li>';
-}
-?>
-        </ul>
-      </div> <!-- .navbar-collapse -->
-    </div> <!-- .container -->
-  </nav>
-</header>
+          <div class="collapse navbar-collapse" id="navbarSupport">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="AddFeedback.php">Feedback</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.php">About Us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="doctors.php">Doctors</a>
+              </li>
+              <?php
+              if (!isset($_SESSION["user_id"]) || empty($_SESSION["user_id"])) {
+                echo '<li class="nav-item"><a class="btn btn-primary ml-lg-3" href="login.php">Login / Register</a></li>';
+              } else {
+                echo '<li class="nav-item"><a class="nav-link" href="#">username:';
+                echo $_SESSION["username"];
+                echo '</a></li>';
+                echo '<li class="nav-item"><a class="btn btn-primary ml-lg-3" href="logout.php">LogOut</a></li>';
+              }
+              ?>
+            </ul>
+          </div> <!-- .navbar-collapse -->
+        </div> <!-- .container -->
+      </nav>
+    </header>
     <h1 id="formTitle">Feedback</h1><br>
     <form action="" method="POST">
-        <label for="name" id="nameTitle">User_id:</label>
-        <?php
-        if(!isset($_SESSION["user_id"]) || empty($_SESSION["user_id"]))
-        {
-          echo '<span id="user_id">You must be logged in.</span></br>';
-          echo '<label  id="nameTitle" style="position:relative;top:15px;">Reviews from other users:</label>
+      <label for="name" id="nameTitle">User_id:</label>
+      <?php
+      if (!isset($_SESSION["user_id"]) || empty($_SESSION["user_id"])) {
+        echo '<span id="user_id">You must be logged in.</span></br>';
+        echo '<label  id="nameTitle" style="position:relative;top:15px;">Reviews from other users:</label>
+        
           <div class="container">
             <div class="row justify-content-center">
               <div class="col-lg-10">
       
-                <div class="row">';
-                  foreach($positive as $feedback)
-                  {
-                    if ($num == 6)
-                    {
-                      break;
-                    }
-                    echo '
-                    <div class="col-md-6 col-lg-4 py-3 wow zoomIn">
+                <div class="row">
+                ';
+        foreach ($positive as $feedback) {
+          if ($num == 6) {
+            break;
+          }
+          echo '
+            <div class="col-md-6 col-lg-4 py-3 wow zoomIn">
               <div class="card-doctor">
+                
                 <div class="body">
-                  <p class="text-xl mb-0">'.$userC->checkUserID($feedback["user_id"])["username"].'</p>
-                  <p class="text-xl mb-0">'.$feedback["review"].' stars</p>
-                  <span class="text-sm text-grey">'.$feedback["description"].'</span>
+                  <p class="text-xl mb-0">' . $userC->checkUserID($feedback["user_id"])["username"] . '</p>
+                  <p class="text-xl mb-0">' . $feedback["review"] . ' stars</p>
+                  <span class="text-sm text-grey">' . $feedback["description"] . '</span>
                 </div>
               </div>
             </div>';
-                  }
-                  
-               echo '</div>
+        }
+
+        echo '
+          </div>
       
               </div>
             </div>
-          </div> <!-- .container -->
+          </div>
         ';
-        }
-        else
-        {
-          echo '<span id="user_id">' . $_SESSION["user_id"] . '</span></br></br>';
-          echo '<label for="review" id="reviewTitle">Review:</label><br />';
-          echo '<div class="rating">
+      } else {
+        echo '<span id="user_id">' . $_SESSION["user_id"] . '</span></br></br>';
+        echo '<label for="review" id="reviewTitle">Review:</label><br />';
+        echo '<div class="rating">
           <label>
           <input type="hidden" name="stars" value=0 />
           </label>
@@ -190,29 +183,28 @@ else
           <span class="icon">★</span>
         </label></div>
         <br />';
-          echo '<label for="feedback" id="feedbackTitle">Feedback:</label><br />
+        echo '<label for="feedback" id="feedbackTitle">Feedback:</label><br />
           <textarea name="description" id="description" rows="5" cols="20" placeholder="Comments Here" required></textarea><br />
           <input type="submit" name="submit" id="submit" value="Submit Feedback" />';
-        }
+      }
 
-        ?>
+      ?>
     </form>
-    </div>
-    <span id="error"><?php echo $error; ?></span>
-    <?php
-    /*if($success == 1)
-    {
-      echo "<div id='popup active'>
-      <h2>Feedback added successfully</h2>
-      <a href='AddFeedback.php'><button id='closePopup' onclick=''>Close</button></a>
-      </div>
-      <script>
-      var blur = document.getElementById('blur');
-      blur.classList.toggle('active');
-      </script>
-      ";
-    }*/?>
-    <footer class="page-footer">
+  </div>
+  <span id="error">
+    <?php echo $error; ?>
+  </span>
+  <?php
+  if ($success == 1) {
+    $showAlert = true;
+
+    // Pass a variable to JavaScript
+    echo '<script>';
+    echo 'var showAlert = ' . json_encode($showAlert) . ';';
+    echo '</script>';
+    $success = 0;
+  } ?>
+  <footer class="page-footer">
     <div class="container">
       <div class="row px-md-3">
         <div class="col-sm-6 col-lg-3 py-3">
@@ -265,7 +257,12 @@ else
         reserved</p>
     </div>
   </footer>
-  
-  
+
+  <script>
+    if (showAlert) {
+      alert('Thank you for your feedback!');
+    }
+  </script>
 </body>
+
 </html>
