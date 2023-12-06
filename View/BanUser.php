14ba42fd81f0ace095efae082052a6d8c216a01a
YassineJanfaoui <47060368+YassineJanfaoui@users.noreply.github.com>
2023-12-07 00:20:07 +0100

    Added mailing functionality to the banning system

--- View/BanUser.php
+++ View/BanUser.php
@@ -3,5 +3,7 @@ session_start();
 include "../Controller/UserC.php";
 $c = new UserController();
 echo $_GET["user_id"];
+$user = $c->checkUserID($_GET["user_id"]);
 $c->banUser($_GET["user_id"]);
+mail($user["email_address"], "Account Banned", "Your account has been banned by the admin.\nPlease contact the admin for more information.");
 header('Location:ListUsers.php');
